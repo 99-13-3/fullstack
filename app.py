c9c5b75a0eedc08082bed1d742151896bf46a37f
Myoung-Been <myoungbeen@gmail.com>
2023-02-11 18:17:56 +0900

    게시글 페이지 이동#1 게시글 수정 기능#1 게시글 수정 기능#1 게시글 삭제 기능#1

--- app.py
+++ app.py
@@ -16,22 +16,57 @@ app = Flask(__name__)
 def home():
    return render_template('index.html')
 
-@app.route('/posting')
+# -----글쓰기 페이지로 이동
+@app.route('/write', )
 def post_upload():
+   memberid = db.fullstack.find_one({'_id:False'})
+   return render_template('indedx.html', memberid = memberid)
 
-   title_receive = request.form['title_give']
-   postdesc_receive = request.form['postdesc_give']
-   postid_receive = request.form['postid_give']
+# -----글쓰기 완료 기능
+@app.route('/write/posting', method=["GET", "POST"])
+def post_upload_done():
+   if request.method == "POST":
 
-   doc = {
-      'title' : title_receive,
-      'postdesc' : postdesc_receive,
-      'postid' : postid_receive
-   }
+      title_receive = request.form['title_give']
+      postdesc_receive = request.form['postdesc_give']
+      memberid_receive = request.form['memberid_give']
 
-   db.fullstack.insert_one(doc)
+      post_list = list(db.fullstack.find({}, {'_id' : False}))
+      post_id = len(post_list) + 1
 
-   return jsonify({'msg': '게시글 등록 완료!'})
+      doc = {
+         'postid' : post_id,
+         'title' : title_receive,
+         'postdesc' : postdesc_receive,
+         'memberid' : memberid_receive,
+         'done' : 0
+      }
+
+      db.fullstack.insert_one(doc)
+
+      return jsonify({'msg': '게시글 등록 완료!'})
+   else:
+      return render_template('index.html')
+
+# -----게시글 수정 기능
+@app.route("/posting", method =["PUT"])
+def post_update():
+   memberid = db.fullstack.find_one({'_id:False'})
+   postid = db.fullstack.find_one({'_id':False})
+   return render_template('index.html', memberid = memberid, postid = postid)
+
+
+# -----게시글 삭제 기능
+# @app.route("/posting", method=["DELETE"])
+# def post_remove():
+#
+#    remove_receive = request.form['remove_give']
+#    db.fullstack.delete_one({'postid' : int(remove_receive)})
+#
+#    return jsonify({'msg' : '게시글 삭제 완료!'})
+
+
+
 
 
 if __name__ == '__main__':
